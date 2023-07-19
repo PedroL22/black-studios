62b86653fcce272014d2af5eefccb900adbb6f5a
Pedro Lucena <pedrocontact22@gmail.com>
2023-07-19 17:00:08 -0300

    💄 styling: Replace Home animations

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,8 +7,8 @@ export const Home = () => {
     <MainContainer>
       <div className='space-y-5 pb-10'>
         <motion.h1
-          initial={{ opacity: 0, x: -15 }}
-          animate={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
           transition={{ duration: 0.5, delay: 0, ease: 'easeOut' }}
           className='flex flex-col whitespace-nowrap text-center text-5xl md:text-9xl'
         >
@@ -16,7 +16,7 @@ export const Home = () => {
           <motion.span
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ duration: 0.75, delay: 0.5, ease: 'easeOut' }}
+            transition={{ duration: 0.5, delay: 0.05, ease: 'easeOut' }}
             className='bg-gradient-to-r from-slate-300 to-slate-600 bg-clip-text text-5xl font-medium text-transparent md:text-9xl'
           >
             Black Studios
@@ -26,16 +26,16 @@ export const Home = () => {
         <motion.h2
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          transition={{ duration: 0.75, delay: 0.6, ease: 'easeOut' }}
+          transition={{ duration: 0.5, delay: 0.1, ease: 'easeOut' }}
           className='text-center text-4xl font-light text-zinc-200 md:text-7xl'
         >
           Produtora audiovisual
         </motion.h2>
       </div>
       <motion.div
-        initial={{ opacity: 0, x: -20 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 0.75, delay: 0.7, ease: 'easeOut' }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.2, ease: 'easeOut' }}
         className='flex justify-center pb-10'
       >
         <iframe
