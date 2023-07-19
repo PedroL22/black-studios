3fe059756e822c535e6e00a2dd3ef71479a8f936
Pedro Lucena <pedrocontact22@gmail.com>
2023-07-19 10:59:13 -0300

    ✨feat: Add new title and description

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export const App = () => {
   return (
     <BrowserRouter>
       <Header />
-      <div className='min-h-screen bg-zinc-900 text-zinc-50'>
+      <div className='min-h-screen bg-zinc-900 text-zinc-50 antialiased'>
         <AllRoutes />
       </div>
     </BrowserRouter>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,11 +4,14 @@ export const Home = () => {
   return (
     <MainContainer>
       <div className='space-y-5'>
-        <h1 className='text-5xl'>
-          Bem-vindos à <span className='text-5xl font-medium'>Black Studios</span>
+        <h1 className='flex flex-col text-center text-5xl md:text-9xl'>
+          Bem-vindos à{' '}
+          <span className='bg-gradient-to-r from-slate-300 to-slate-600 bg-clip-text text-5xl font-medium text-transparent md:text-9xl'>
+            Black Studios
+          </span>
         </h1>
 
-        <h2 className='text-4xl font-light text-zinc-200'>Produtora audiovisual</h2>
+        <h2 className='text-center text-4xl font-light text-zinc-200 md:text-7xl'>Produtora audiovisual</h2>
       </div>
     </MainContainer>
   )
