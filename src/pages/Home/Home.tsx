54205acff55303c31b61c9bf8091c5a2b7787794
Pedro Lucena <pedrocontact22@gmail.com>
2023-07-19 10:28:38 -0300

    ✨feat: Add title and description to home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,15 @@
 import { MainContainer } from '@components/MainContainer'
 
 export const Home = () => {
-  return <MainContainer>Home</MainContainer>
+  return (
+    <MainContainer>
+      <div className='space-y-5'>
+        <h1 className='text-5xl'>
+          Bem-vindos à <span className='text-5xl font-medium'>Black Studios</span>
+        </h1>
+
+        <h2 className='text-4xl font-light text-zinc-200'>Produtora audiovisual</h2>
+      </div>
+    </MainContainer>
+  )
 }
