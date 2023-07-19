5c8b7d7b249f770aee047511e7667c99cdfafd17
Pedro Lucena <pedrocontact22@gmail.com>
2023-07-19 14:11:20 -0300

    ✨feat: Add Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter } from 'react-router-dom'
 import { AllRoutes } from './routes'
 
+import { Footer } from '@components/Footer'
 import { Header } from '@components/Header'
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Header />
-      <div className='min-h-screen bg-zinc-900 text-zinc-50 antialiased'>
+      <div className='min-h-[calc(100vh-8rem)] bg-zinc-900 text-zinc-50 antialiased'>
         <AllRoutes />
       </div>
+      <Footer />
     </BrowserRouter>
   )
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+export const Footer = () => {
+  return (
+    <footer className='h-16 bg-zinc-950'>
+      <div className='mx-auto w-80 sm:w-[28rem] md:w-[36rem] lg:w-[55rem]'>
+        <div className='mx-auto flex justify-between px-16 pt-4 md:px-36 lg:px-72' />
+        <p className='pt-2 text-center text-xs text-zinc-50'>
+          Made with <span className='text-red-600'>♥</span> by{' '}
+          <a
+            href='https://github.com/PedroL22/'
+            target='_blank'
+            rel='noreferrer'
+            className='font-medium text-zinc-50 no-underline'
+          >
+            Pedro Lucena
+          </a>
+        </p>
+      </div>
+    </footer>
+  )
+}
